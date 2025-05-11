7f9f750d38eaff15d76f61ef708cecb2be54389a
Lasha Shavgulidze <lasha.shavgulidze@caucasusauto.com>
2025-05-11 13:52:02 +0400

    Feature/Dashboard

---
+++ src/app/shared/components/layout/layout.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavigationService } from '../../../core/services/navigation.service';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SidebarComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {
+  private navService = inject(NavigationService);
+  sidebarExpanded = true;
+
+  ngOnInit(): void {
+    this.navService.expanded$.subscribe((expanded) => {
+      this.sidebarExpanded = expanded;
+    });
+  }
+}
