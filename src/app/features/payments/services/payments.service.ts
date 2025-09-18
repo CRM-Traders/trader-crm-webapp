d046cc0deef38902777057b46196b7a772ebc552
nikoloz kuridze <nikolozkuridze21@gmail.com>
2025-09-18 20:57:20 +0400

    Update payment statistic endpoint

--- src/app/features/payments/services/payments.service.ts
+++ src/app/features/payments/services/payments.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class PaymentsService {
-  private apiUrl = '/api/payments';
+  private apiUrl = '/identity/api/payments';
 
   constructor(private http: HttpClient) {}
 
